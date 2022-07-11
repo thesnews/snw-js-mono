9291407c1b675a2e76c00a89bf229db7df6903cb
Mike Joseph <mike.joseph5@gmail.com>
2022-07-11 12:32:00 -0400

    fix refs update origin check fix back/forward on sideloader

--- side-loader/v2/src/index.js
+++ side-loader/v2/src/index.js
@@ -1,90 +1,104 @@
-window._snw_audio_player_v2 = function (d, w, i) {
+window._snw_side_loader_v2 = function (w, d, l, h) {
+    var ORIG = window.location.origin;
+
+    function init() {
+        scanLinks(null);
+        window.addEventListener("popstate", function () {
+            if (window.location.href.indexOf(ORIG) === 0) {
+                loadUrl(window.location.href, false);
+            }
+        });
+    }
+
+    function scanLinks(parent) {
+        var baseUrl = l.protocol + "//" + l.host;
+        var doc = d;
+        if (parent !== undefined && parent !== null) {
+            var doc = d.querySelector(parent);
+        }
+
+        var links = doc.querySelectorAll('a[href^="' + baseUrl + '"]');
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].getAttribute("data-href") !== null) {
+                continue;
+            }
+            links[i].addEventListener("click", handleInternalLink);
+            links[i].setAttribute("data-href", links[i].href);
+        }
+    }
+
+    function handleInternalLink(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var url = this.getAttribute("data-href");
+        loadUrl(url, true);
+    }
+
+    function loadUrl(url, pushState) {
+        const headers = new Headers();
+        headers.append(
+            "Accept",
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        );
+        headers.append("Accept-Encoding", "gzip, deflate, br");
+        headers.append("User-Agent", w.navigator.userAgent);
+
+        const req = new Request(url);
+        d.body.style.cursor = "wait";
+
+        fetch(req, {
+            method: "GET",
+            headers: headers,
+        })
+            .then(function (resp) {
+                return resp.text();
+            })
+            .then(function (str) {
+                try {
+                    replace(
+                        str,
+                        url
+                            .replace(l.protocol + "//" + l.host, "")
+                            .replace(/^\/{2,}/, "/")
+                    );
+                    if (pushState) {
+                        h.pushState({}, null, url);
+                    }
+                } catch (e) {
+                    throw new Error(e);
+                }
+            })
+            .catch(function (err) {
+                // failed to load, so just navigate to the page
+                w.location.href = url;
+                d.body.style.cursor = "inherit";
+            });
+    }
+
+    function replace(str, url) {
+        var p = new DOMParser();
+        var doc = p.parseFromString(str, "text/html");
+        var nodes = doc.querySelectorAll("[data-replaceable-content='true']");
+        for (var i = 0; i < nodes.length; i++) {
+            var node = nodes[i];
+            var id = node.getAttribute("id");
+
+            var nodeToReplace = d.getElementById(id);
+            while (nodeToReplace.firstChild) {
+                nodeToReplace.removeChild(nodeToReplace.firstChild);
+            }
+
+            for (var j = 0; j < node.childNodes.length; j++) {
+                nodeToReplace.appendChild(node.childNodes[j].cloneNode(true));
+            }
+        }
+
+        w.scrollTo(0, 0);
+        d.body.style.cursor = "inherit";
+        scanLinks("#" + id);
+    }
+
     d.addEventListener("DOMContentLoaded", function () {
-        var container = d.getElementById(i);
-        var player = d.querySelector("audio");
-        player.addEventListener("playing", function () {
-            console.log("playing...");
-            if (container.querySelector("small .fa-circle-notch")) {
-                b = container.querySelector("small .fa-circle-notch");
-                b.classList.remove("fa-circle-notch", "fa-spin");
-                b.classList.add("fa-pause");
-            } else if (container.querySelector("small .fa-play")) {
-                b = container.querySelector("small .fa-play");
-                b.classList.remove("fa-play");
-                b.classList.add("fa-pause");
-            }
-        });
-
-        player.addEventListener("waiting", function () {
-            console.log("waiting...");
-            if (container.querySelector("small .fa-play")) {
-                b = container.querySelector("small .fa-play");
-                b.classList.remove("fa-play");
-                b.classList.add("fa-circle-notch", "fa-spin");
-            } else if (container.querySelector("small .fa-pause")) {
-                b = container.querySelector("small .fa-pause");
-                b.classList.remove("fa-pause");
-                b.classList.add("fa-circle-notch", "fa-spin");
-            }
-        });
-
-        player.addEventListener("pause", function () {
-            console.log("paused...");
-            if (container.querySelector("small .fa-pause")) {
-                b = container.querySelector("small .fa-pause");
-                b.classList.remove("fa-pause");
-                b.classList.add("fa-play");
-            } else if (container.querySelector("small .fa-circle-notch")) {
-                b = container.querySelector("small .fa-circle-notch");
-                b.classList.remove("fa-circle-notch", "fa-spin");
-                b.classList.add("fa-pause");
-            }
-        });
-
-        container
-            .querySelector("[data-player-action='play-pause']")
-            .addEventListener("click", function (e) {
-                e.preventDefault();
-                var b;
-                if (player.paused) {
-                    player.play();
-                } else {
-                    player.pause();
-                }
-            });
-        container
-            .querySelector("[data-player-action='vol-down']")
-            .addEventListener("click", function (e) {
-                e.preventDefault();
-                if (parseFloat(player.volume) === 0) {
-                    return;
-                }
-                player.volume = parseFloat(player.volume) - 0.1;
-                container.querySelector(
-                    "[data-player-action='vol-range']"
-                ).value = player.volume;
-            });
-        container
-            .querySelector("[data-player-action='vol-up']")
-            .addEventListener("click", function (e) {
-                e.preventDefault();
-                if (parseFloat(player.volume) === 1) {
-                    return;
-                }
-                player.volume = parseFloat(player.volume) + 0.1;
-                container.querySelector(
-                    "[data-player-action='vol-range']"
-                ).value = player.volume;
-            });
-        container
-            .querySelector("[data-player-action='vol-range']")
-            .addEventListener("change", function (e) {
-                e.preventDefault();
-                var v = parseFloat(this.value);
-                if (v > 1 || v < 0) {
-                    return;
-                }
-                player.volume = v;
-            });
+        init();
     });
 };
